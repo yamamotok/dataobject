a7ad5a8e5b299f855e16e2e923bbe0fb8626058c
Keisuke Yamamoto <keisuke@modelhouse.tech>
2021-01-10 06:38:03 +0900

    Change context name (toJson -> toPlain) in test cases

--- src/PropertyDecoratorOptions.test.ts
+++ src/PropertyDecoratorOptions.test.ts
@@ -7,15 +7,15 @@ describe('PropertyDecoratorOptions', () => {
   });
 
   test('Context matching, in case expected a single context', () => {
-    const expected = 'toJson';
+    const expected = 'toPlain';
     expect(inContext('response', expected)).toBe(false);
-    expect(inContext('toJson', expected)).toBe(true);
+    expect(inContext('toPlain', expected)).toBe(true);
   });
 
   test('Context matching, in case expected multiple context', () => {
-    const expected = ['toJson', 'factory', 'response'];
+    const expected = ['toPlain', 'factory', 'response'];
     expect(inContext('response', expected)).toBe(true);
-    expect(inContext('toJson', expected)).toBe(true);
+    expect(inContext('toPlain', expected)).toBe(true);
     expect(inContext('factory', expected)).toBe(true);
     expect(inContext('unknown', expected)).toBe(false);
   });
@@ -23,13 +23,13 @@ describe('PropertyDecoratorOptions', () => {
   test('Context matching, in case no current context was given', () => {
     expect(inContext(undefined, undefined)).toBe(true);
     expect(inContext(undefined, 'undefined')).toBe(true);
-    expect(inContext(undefined, ['undefined', 'toJson', 'factory'])).toBe(true);
+    expect(inContext(undefined, ['undefined', 'toPlain', 'factory'])).toBe(true);
   });
 
   test('Context matching, simple negation case', () => {
     const expected = '!response';
     expect(inContext('response', expected)).toBe(false);
-    expect(inContext('toJson', expected)).toBe(true);
+    expect(inContext('toPlain', expected)).toBe(true);
     expect(inContext('factory', expected)).toBe(true);
   });
 
@@ -37,15 +37,15 @@ describe('PropertyDecoratorOptions', () => {
     const expected = ['!response', '!persistence'];
     expect(inContext('response', expected)).toBe(false);
     expect(inContext('persistence', expected)).toBe(false);
-    expect(inContext('toJson', expected)).toBe(true);
+    expect(inContext('toPlain', expected)).toBe(true);
     expect(inContext('factory', expected)).toBe(true);
   });
 
   test('Context matching, complex negation case with some acceptance', () => {
-    const expected = ['!response', '!persistence', 'toJson'];
+    const expected = ['!response', '!persistence', 'toPlain'];
     expect(inContext('response', expected)).toBe(false);
     expect(inContext('persistence', expected)).toBe(false);
-    expect(inContext('toJson', expected)).toBe(true);
+    expect(inContext('toPlain', expected)).toBe(true);
     expect(inContext('factory', expected)).toBe(false);
   });
 });
